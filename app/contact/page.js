bf71a49f9241200ee57ffb94362b716fdf0490e1
MRsteeds14 <SOMMERSJ1129@GMAIL.COM>
2025-08-12 12:01:44 -0400

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -36,7 +36,7 @@ export default function ContactPage() {
           <img
             src="/MS Services Logo.png"
             alt="MS Services Logo – Click to contact Energy Lady NJ"
-            className="hero-logo mx-auto h-32 w-32 cursor-pointer object-contain transition-transform hover:scale-105"
+            className=".logo-image mx-auto"
           />
         </a>
       </div>
